3f8135d31e7fc9659e584fe61058ee82813e2354
Oliver Deng <oliver.t.deng@gmail.com>
2026-05-02 14:32:42 -0400

    refactor: change to correct paths

--- cmake/SourcesAndHeaders.cmake
+++ cmake/SourcesAndHeaders.cmake
@@ -8,7 +8,7 @@ set(exe_sources
 )
 
 set(headers
-    include/project/tmp.hpp
+    include/obxx/tmp.hpp
 )
 
 set(test_sources
--- test/src/tmp_test.cpp
+++ test/src/tmp_test.cpp
@@ -4,7 +4,7 @@
 
 TEST(TmpAddTest, CheckValues)
 {
-  ASSERT_EQ(tmp::add(1, 2), 3);
+  // ASSERT_EQ(obxx::add(1, 2), 3);
   EXPECT_TRUE(true);
 }
 
