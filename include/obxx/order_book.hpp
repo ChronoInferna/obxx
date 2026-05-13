a330749e28713a116f16d4c4938d462365818ac9
Oliver Deng <oliver.t.deng@gmail.com>
2026-05-12 21:10:31 -0400

    feat: introduce interaction with order

--- include/obxx/order_book.hpp
+++ include/obxx/order_book.hpp
@@ -13,11 +13,13 @@ namespace obxx
 
   struct PriceLevel
   {
-    std::vector<Order> orders;
-    OrderVolume volume;
-    uint64_t order_quantity;
+    std::vector<OrderId> orders;
+    OrderQuantity volume;
+    uint64_t trade_quantity;
+    std::unordered_map<OrderId, std::unique_ptr<Order>>* orders_ptr;  // Pointer for O(1)
 
-    void add_order(Order& order);
+    void add_order(OrderId id);
+    // std::optional<Order> fill_order(Order& order);
     // TODO
   };
 
@@ -26,35 +28,33 @@ namespace obxx
     OrderId taker;
     OrderId maker;
     Decimal<2> price;
-    OrderVolume volume;
+    OrderQuantity volume;
   };
 
   struct Update
   {
     Decimal<2> price;
-    OrderVolume volume;
+    OrderQuantity volume;
     OrderSide side;
   };
 
   // TODO how to notify when order is filled?
   // TODO decide what kinds of updates to expose
+  // TODO move out of this file
   using Event = std::variant<Trade, Update>;
 
   class OrderBook
   {
    private:
-    // TODO best data structure for this? std::flat_map? Only in GCC 15 though
     std::unordered_map<Decimal<2>, PriceLevel> bids_;
     std::unordered_map<Decimal<2>, PriceLevel> asks_;
-
-    // TODO if order > volume at price level, we can just clear and mark all as filled
-    void clear_bids();
-    void clear_asks();
+    // Pointer to orders for O(1) cancel
+    std::unordered_map<OrderId, std::unique_ptr<Order>> orders_;
 
    public:
     // Input
-    OrderId submit_order_request(OrderRequest& request);
-    void cancel_order(OrderId id);
+    std::expected<OrderId, std::string> submit_order_request(OrderRequest& request);
+    std::expected<void, std::string> cancel_order(OrderId id);
 
     // Ouput
     std::vector<Event> poll_events(int num_requests) const;  // How to determine how many requests to give?
@@ -63,7 +63,24 @@ namespace obxx
     std::optional<Decimal<2>> best_bid() const;
     std::optional<Decimal<2>> best_ask() const;
     std::optional<Order> query_order_id(OrderId id) const;
-    OrderVolume volume_at_price(Decimal<2> price) const;
+    std::optional<OrderQuantity> volume_at_price(Decimal<2> price) const;
+
+   private:
+    OrderId create_order(OrderRequest& request);
+    void add_order_to_bids(OrderId id);
+    void add_order_to_asks(OrderId id);
+
+    // TODO if order > volume at price level, we can just clear and mark all as filled
+    void clear_bids(Decimal<2> price);
+    void clear_asks(Decimal<2> price);
+
+    std::expected<OrderId, std::string> submit_buy_request(OrderRequest& request);
+    std::expected<OrderId, std::string> submit_market_buy_request(OrderRequest& request);
+    // TODO add more order types
+
+    std::expected<OrderId, std::string> submit_sell_request(OrderRequest& request);
+    std::expected<OrderId, std::string> submit_market_sell_request(OrderRequest& request);
+    // TODO add more order types
   };
 
 }  // namespace obxx
--- src/order_book.cpp
+++ src/order_book.cpp
@@ -3,9 +3,113 @@
 namespace obxx
 {
 
-  void PriceLevel::add_order(Order& order)
+  void PriceLevel::add_order(OrderId id)
   {
-    orders.emplace_back(std::move(order));
+    orders.push_back(id);
+    volume += (*orders_ptr)[id]->quantity;
+    trade_quantity++;
+  }
+
+  OrderId OrderBook::create_order(OrderRequest& request)
+  {
+    auto order = std::make_unique<Order>(request);
+    OrderId order_id = OrderIdGenerator::next();
+    orders_[order_id] = std::move(order);
+    return order_id;
+  }
+
+  void OrderBook::add_order_to_bids(OrderId id)
+  {
+    Decimal<2> price = orders_[id]->price.value();
+    if (bids_.find(price) == bids_.end())
+    {
+      bids_[price] = PriceLevel{ {}, 0, 0, &orders_ };
+    }
+    bids_[price].add_order(id);
+  }
+
+  void OrderBook::add_order_to_asks(OrderId id)
+  {
+    Decimal<2> price = orders_[id]->price.value();
+    if (asks_.find(price) == asks_.end())
+    {
+      asks_[price] = PriceLevel{ {}, 0, 0, &orders_ };
+    }
+    bids_[price].add_order(id);
+  }
+
+  std::expected<OrderId, std::string> OrderBook::submit_order_request(OrderRequest& request)
+  {
+    if (request.side == OrderSide::Buy)
+    {
+      return submit_buy_request(request);
+    }
+    else
+    {
+      return submit_sell_request(request);
+    }
+  }
+
+  std::expected<OrderId, std::string> OrderBook::submit_buy_request(OrderRequest& request)
+  {
+    switch (request.type)
+    {
+      case OrderType::Market: return submit_market_buy_request(request);
+      case OrderType::Limit:
+        // TODO
+        return std::unexpected("Unimplemented, cannot submit limit buy request");
+      case OrderType::Stop:
+        // TODO
+        return std::unexpected("Unimplemented, cannot submit stop buy request");
+      case OrderType::StopLimit:
+        // TODO
+        return std::unexpected("Unimplemented, cannot submit stop limit buy request");
+      default: return std::unexpected("Invalid order type");
+    }
+  }
+
+  std::expected<OrderId, std::string> OrderBook::submit_market_buy_request(OrderRequest& request)
+  {
+    OrderId id = create_order(request);
+
+    // Determine if it fills
+    if (bids_.empty())
+    {
+      orders_[id]->status = OrderStatus::Rejected;
+    }
+    else  // Fill
+    {
+      for (auto& [price, price_level] : asks_)
+      {
+        if (orders_[id]->quantity <= price_level.volume)
+        {
+          // Fill order and update price level
+          price_level.volume -= orders_[id]->quantity;
+          orders_[id]->status = OrderStatus::Filled;
+          break;
+        }
+        else
+        {
+          // Partially fill order and remove price level
+          orders_[id]->quantity -= price_level.volume;
+          price_level.volume = 0;
+          price_level.orders.clear();
+        }
+      }
+    }
+
+    return id;
+  }
+
+  std::expected<OrderId, std::string> OrderBook::submit_sell_request(OrderRequest& request)
+  {
+    Order order{ request };
+    return std::unexpected("Unimplemented, cannot submit sell request");
+  }
+
+  std::expected<void, std::string> OrderBook::cancel_order(OrderId id)
+  {
+    return std::unexpected("Unimplemented, cannot cancel order " + std::to_string(id));
   }
 
 }  // namespace obxx
