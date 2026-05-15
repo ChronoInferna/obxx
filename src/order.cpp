db92c418ed2d1db11045b61007a29a35afe03c4c
Oliver Deng <oliver.t.deng@gmail.com>
2026-05-14 21:19:02 -0400

    feat: string version of order status and reordered orders to be type, side, quantity

--- src/order.cpp
+++ src/order.cpp
@@ -3,6 +3,20 @@
 namespace obxx
 {
 
+  std::string_view to_string(OrderStatus status)
+  {
+    switch (status)
+    {
+      case OrderStatus::Unfilled: return "Unfilled";
+      case OrderStatus::PartiallyFilled: return "PartiallyFilled";
+      case OrderStatus::Filled: return "Filled";
+      case OrderStatus::Canceled: return "Canceled";
+      case OrderStatus::Rejected: return "Rejected";
+      case OrderStatus::Expired: return "Expired";
+      default: return "Unknown";
+    }
+  }
+
   // TODO: Implement a more robust ID generator that can handle overflow and persistence - also a better ID system
   OrderId OrderIdGenerator::next()
   {
@@ -11,16 +25,16 @@ namespace obxx
 
   Order::Order(const OrderRequest& request)
       : status(OrderStatus::Unfilled),
+        type(request.type),
         side(request.side),
         quantity(request.quantity),
-        type(request.type),
         price(request.price)
   {
   }
 
-  OrderRequestBuilder::OrderRequestBuilder(OrderSide side, OrderQuantity quantity, OrderType type)
+  OrderRequestBuilder::OrderRequestBuilder(OrderType type, OrderSide side, OrderQuantity quantity)
   {
-    request_ = std::make_unique<OrderRequest>(side, quantity, type);
+    request_ = std::make_unique<OrderRequest>(type, side, quantity);
   }
 
   std::expected<OrderRequest, std::string> OrderRequestBuilder::build()
@@ -44,10 +58,10 @@ namespace obxx
     return *request_;
   }
 
-  OrderRequest& OrderRequestBuilder::price(Decimal<2> price)
+  OrderRequestBuilder& OrderRequestBuilder::price(Decimal<2> price)
   {
     request_->price = price;
-    return *request_;
+    return *this;
   }
 
 }  // namespace obxx
